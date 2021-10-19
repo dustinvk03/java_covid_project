1a68bdfa5ba0536daa17df1063f9807ba9c03258
dustinvk03 <dustinvk03@gmail.com>
2021-10-18 21:32:56 -0500

    Add USA and main CovidInfo. Test with hand input data for USA

---
+++ CovidInfo/src/CovidReport.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class CovidReport {
+	
+	private int regionCode;
+	private int requestCode;
+
+	public static void main(String[] args) {
+		
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter request code: Retrieve - 1 | Modify - 2");		
+		int requestCode = scan.nextInt();
+		
+		// If 1 => retrieve data
+		
+		System.out.print("Enter region code: World - 1 | USA - 2 | WSU - 3");
+		int regionCode = scan.nextInt();
+		
+		USA usa = new USA();
+	
+		if (regionCode == 2) {
+			
+		
+			int cases = usa.getnumcases();
+			int dealth = usa.getnumdealth();
+			
+			
+		
+			System.out.println("");
+			System.out.println("---------USA COVID REPORT---------");
+			
+			System.out.println("Total cases: " + cases);
+			System.out.println("Total dealth: " + dealth);
+			System.out.println("-----------------------------------");
+		
+		}
+		
+		
+
+	}
+
+}
---
+++ CovidInfo/src/USA.java
@@ -0,0 +1,20 @@
+
+public class USA {
+	
+
+	private int numcases = 44900000;
+	private int numdealth = 724000;		
+		
+	
+	
+	public int getnumcases() {
+		return numcases;
+		
+	}
+	
+	public int getnumdealth() {
+		return numdealth;
+		
+	}
+	
+}
