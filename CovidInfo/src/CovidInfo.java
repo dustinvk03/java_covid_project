fa019cd487b4d44ed510a5e83c8172c32dbbf33d
dustinvk03 <dustinvk03@gmail.com>
2021-10-24 22:00:09 -0500

    Just "connect" CovidInfo to GUI. Easy part done.

--- CovidInfo/src/CovidInfo.java
+++ CovidInfo/src/CovidInfo.java
@@ -11,69 +11,68 @@ public class CovidInfo {
 	
 	
 	public int RetrieveCase(int regionCode) {
-		return dataArray[regionCode-1][0];
+		return dataArray[regionCode][0];
 
 		}
 	
 	
 	public int RetrieveDealth(int regionCode) {
-		return dataArray[regionCode-1][1];
+		return dataArray[regionCode][1];
 
 		}
 	
 
-	
-	public static void main(String[] args) {
-
-		Scanner scan = new Scanner(System.in);
-		
-		int requestCode = 0;
-		
-		System.out.println("Enter your request code: Retrieve - 1 | Modify - 2");
-		
-		while (true) {
-			
-			requestCode = scan.nextInt();
-			if ((requestCode == 1) || (requestCode == 2) || (requestCode == 0))
-				
-				break;
-			else 
-				System.out.println("request code is wrong, please type again");
-
-		
-		}
-		
-		if (requestCode == 1) {
-
-			
-		
-			System.out.println("Enter your region code: World - 1 | USA - 2 | WSU - 3");
-			
-			int code = scan.nextInt();
-			
-			CovidInfo app = new CovidInfo();
-			
-			
-				
-			if (code == 1) { 
-				System.out.println("World has " + app.RetrieveCase(code) + " cases and " + app.RetrieveDealth(code) + " dealths");
-			}
-			else if (code == 2) { 
-				System.out.println("USA has " + app.RetrieveCase(code) + " cases and " + app.RetrieveDealth(code) + " dealths");
-			}
-			else if (code == 3) {
-				System.out.println("WSU has " + app.RetrieveCase(code) + " cases and " + app.RetrieveDealth(code) + " dealths");
-	
-			}
-		
-		
-		}
-		
-		
-		if (requestCode == 2) {
-			
-			
-		}
-	
-	}
+//	public static void main(String[] args) {
+//
+//		Scanner scan = new Scanner(System.in);
+//		
+//		int requestCode = 0;
+//		
+//		System.out.println("Enter your request code: Retrieve - 1 | Modify - 2");
+//		
+//		while (true) {
+//			
+//			requestCode = scan.nextInt();
+//			if ((requestCode == 1) || (requestCode == 2) || (requestCode == 0))
+//				
+//				break;
+//			else 
+//				System.out.println("request code is wrong, please type again");
+//
+//		
+//		}
+//		
+//		if (requestCode == 1) {
+//
+//			
+//		
+//			System.out.println("Enter your region code: World - 1 | USA - 2 | WSU - 3");
+//			
+//			int code = scan.nextInt();
+//			
+//			CovidInfo app = new CovidInfo();
+//			
+//			
+//				
+//			if (code == 0) { 
+//				System.out.println("World has " + app.RetrieveCase(code) + " cases and " + app.RetrieveDealth(code) + " dealths");
+//			}
+//			else if (code == 1) { 
+//				System.out.println("USA has " + app.RetrieveCase(code) + " cases and " + app.RetrieveDealth(code) + " dealths");
+//			}
+//			else if (code == 2) {
+//				System.out.println("WSU has " + app.RetrieveCase(code) + " cases and " + app.RetrieveDealth(code) + " dealths");
+//	
+//			}
+//		
+//		
+//		}
+//		
+//		
+//		if (requestCode == 2) {
+//			
+//			
+//		}
+//	
+//	}
 }
--- CovidInfo/src/CovidInfoGUI.java
+++ CovidInfo/src/CovidInfoGUI.java
@@ -10,7 +10,7 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 
-public class CovidInfoGUI extends JFrame{
+public class CovidInfoGUI extends JFrame implements ActionListener {
     private JLabel lblRequest;    //show "Enter the input length (km): "
     private JComboBox cbbRequest; //text box to input the length 
     private JLabel lblRegion; //show the text "Choose the units to convert"
@@ -56,19 +56,18 @@ public class CovidInfoGUI extends JFrame{
     	
     	lblRegion = new JLabel("Select the target regions:");
     	
-    	   	
-    	
+
         //Create the string arrays of units
         String[] regions = new String[] {"World", "USA", "WSU"};
         //create the combobox for user to select the target unit
         cbbRegion = new JComboBox(regions);
         //init the button btnConvert with the text "Convert"
         
-        btnExecute = new JButton("Do!");
+        btnExecute = new JButton("Go!");
         
         //init the text area with 5 rows, 20 columns
         
-        txtResult = new JTextArea(5, 20);
+        txtResult = new JTextArea(5, 25);
                 
         //add those components to the form
         add(lblRequest);
@@ -82,17 +81,63 @@ public class CovidInfoGUI extends JFrame{
         setVisible(true);
         //register event handler of "Report" button
 //        
-//        btnConvert.addActionListener(this);
+        btnExecute.addActionListener(this);
         
     }
 
-    /* the main method */
-    public static void main ( String args [] ) {
-        new CovidInfoGUI();
-    }
 
-    /* handle user click events */
-  
+
+
+	/* the main method */
+	public static void main ( String args [] ) {
+	    new CovidInfoGUI();
+	}
+	
+	/* handle user click events */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+	    //Implement the event when user click on the Convert button
+	    //Check if "Convert" button was clicked
+		
+	    if(e.getSource() == btnExecute) {
+	        //clear the text in the txtResult by calling setText method with empty string
+	        
+	    	txtResult.setText("");
+	    	
+	        //int sUnit = get the selected index of target unit in the combo box cbbDestUnit
+	        
+	    	int sUnit = cbbRegion.getSelectedIndex();
+	    	
+	        //use if-else on the selected target unit to convert the input length
+	
+	        // to the target unit by using the static methods in the UnitConverter class
+	        // and append to text box txtResult
+	    	
+	    	CovidInfo app = new CovidInfo();
+	    	
+	        if (sUnit == 0) { //select "World"
+	        		        	       	
+	            txtResult.append("World has " + app.RetrieveCase(0) + " cases and " +
+	            app.RetrieveDealth(0) + " dealths");
+	        }
+	            
+	         else if (sUnit == 1) { //select "USA"
+	        	 
+	        	 txtResult.append("USA has " + app.RetrieveCase(1) + " cases and " +
+	        	 app.RetrieveDealth(1) + " dealths");
+	        	 
+	         }
+	        
+	         else if (sUnit == 2) { //select "WSU"
+	        	
+	        	 txtResult.append("WSU has " + app.RetrieveCase(2) + " cases and " +
+	    	        	 app.RetrieveDealth(2) + " dealths");
+	        	 
+	        	 
+	         }
+	    
+	  
+	}
+	        	
+} 
 }
-        	
-  
